bd0ec10826c28f3ade0cac205526e6d75125bbef
kacper.krasnodebski <k.krasnodebskit@gmail.com>
2023-05-28 22:45:29 +0200

    simple API created

--- index.ts
+++ index.ts
@@ -1,15 +1,22 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { connectToDatabase } from './database/database.service';
+import { gamesRouter } from './routes/games.routes';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
-
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+connectToDatabase()
+    .then(() => {
+        app.use("/games", gamesRouter);
+        
+        app.listen(port, () => {
+            console.log(`Server started at http://localhost:${port}`);
+        });
+    })
+    .catch((error: Error) => {
+        console.error("Database connection failed", error);
+        process.exit();
+    });
